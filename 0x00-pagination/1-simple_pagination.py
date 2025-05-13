583959f1101f1b1591ca09f2b8e0b3bac41654ab
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-13 05:09:01 +0100

    Define server class

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+import csv
+import math
+from typing import List
+
+
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get items list of page range
+        """
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+
+        list_range = index_range(page, page_size)
+        self.dataset()
+        if list_range[0] >= len(self.__dataset):
+            return []
+
+        return self.__dataset[list_range[0]:list_range[1]]
