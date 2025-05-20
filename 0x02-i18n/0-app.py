17553d534b74d065ab2e80149b3af4507c669f3c
Johnkenzzy <johnkenumeh@gmail.com>
2025-05-20 14:10:37 +0100

    Type annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,15 +3,18 @@
 """
 
 from flask import Flask, render_template
+from typing import Union
+from werkzeug import Response
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> Union[str, Response]:
+    """Index route"""
     return render_template('0-index.html')
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
